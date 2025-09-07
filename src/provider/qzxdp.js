e8ec8b95be4604d9a83b0cf371c1ca6a4d896406
OASEN <71011762+A233S@users.noreply.github.com>
2025-09-07 11:51:05 +0800

    Update qzxdp.js

--- src/provider/qzxdp.js
+++ src/provider/qzxdp.js
@@ -7,7 +7,24 @@ const track = (info) => {
 	const targetUrl = `https://music.163.com/#/song?id=${info.id}&musicType=lossless`;
 	
 	return request('POST', apiUrl, {
-		headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+		"headers": {
+		  "accept": "application/json, text/javascript, */*; q=0.01",
+		  "accept-language": "zh-CN,zh;q=0.9,en;q=0.8",
+		  "cache-control": "no-cache",
+		  "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+		  "pragma": "no-cache",
+		  "priority": "u=1, i",
+		  "sec-ch-ua": "\"Chromium\";v=\"128\", \"Not;A=Brand\";v=\"24\", \"Google Chrome\";v=\"128\"",
+		  "sec-ch-ua-mobile": "?0",
+		  "sec-ch-ua-platform": "\"Windows\"",
+		  "sec-fetch-dest": "empty",
+		  "sec-fetch-mode": "cors",
+		  "sec-fetch-site": "same-origin",
+		  "x-requested-with": "XMLHttpRequest",
+		  "cookie": "__gads=ID=ff436b87de5bf6c0:T=1747848200:RT=1747848200:S=ALNI_MZ2FsvktEWrcdwhbSP9xr20G4y-Tg; __eoi=ID=6fa59cd04e8b71ff:T=1747848200:RT=1747848200:S=AA-AfjaHt8NfABRt6mCksneltsNB; PHPSESSID=9d4cc575bef2f0a74c17297fc6238015",
+		  "Referer": "https://tools.qzxdp.cn/wyy_vip",
+		  "Referrer-Policy": "strict-origin-when-cross-origin"
+		},
 		body: `url=${encodeURIComponent(targetUrl)}`
 	})
 		.then((response) => response.json())
