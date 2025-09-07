e090e83a1c3c356660283dfcca2f10933f12e890
OASEN <71011762+A233S@users.noreply.github.com>
2025-09-07 12:50:04 +0800

    Update qzxdp.js

--- src/provider/qzxdp.js
+++ src/provider/qzxdp.js
@@ -1,3 +1,4 @@
+const util = require('util');
 const select = require('./select');
 const request = require('../request');
 const { getManagedCacheStorage } = require('../cache');
@@ -5,6 +6,7 @@ const { getManagedCacheStorage } = require('../cache');
 const track = (info) => {
 	const apiUrl = 'https://tools.qzxdp.cn/api/wyy_vip/parse';
 	const targetUrl = `https://music.163.com/#/song?id=${info.id}&musicType=lossless`;
+	console.log(targetUrl);
 	
 	return request('POST', apiUrl, {
 		"headers": {
@@ -27,6 +29,7 @@ const track = (info) => {
 		},
 		body: `url=${encodeURIComponent(targetUrl)}`
 	})
+		console.log(response);
 		.then((response) => response.json())
 		.then((jsonBody) => {
 			if (jsonBody.status === 'ok' && jsonBody.data?.url) {
