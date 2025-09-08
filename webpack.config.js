ac2063342217ffab7d37bc0e5d5a531f99a76dd5
OASEN <71011762+A233S@users.noreply.github.com>
2025-09-08 21:32:02 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,55 @@ module.exports = {
 		filename: '[name].js',
 		path: __dirname + '/precompiled',
 	},
+	// 添加 externals 来排除 Node.js 内置模块
+	externals: {
+		'fs': 'commonjs fs',
+		'path': 'commonjs path',
+		'http': 'commonjs http',
+		'https': 'commonjs https',
+		'crypto': 'commonjs crypto',
+		'stream': 'commonjs stream',
+		'util': 'commonjs util',
+		'url': 'commonjs url',
+		'querystring': 'commonjs querystring',
+		'os': 'commonjs os',
+		'net': 'commonjs net',
+		'tls': 'commonjs tls',
+		'zlib': 'commonjs zlib',
+		'events': 'commonjs events',
+		'buffer': 'commonjs buffer',
+		'child_process': 'commonjs child_process',
+		'cluster': 'commonjs cluster',
+		'dgram': 'commonjs dgram',
+		'dns': 'commonjs dns',
+		'readline': 'commonjs readline',
+		'repl': 'commonjs repl',
+		'tty': 'commonjs tty',
+		'vm': 'commonjs vm',
+		'worker_threads': 'commonjs worker_threads'
+	},
+	// 添加 resolve 配置
+	resolve: {
+		extensions: ['.js', '.json'],
+		fallback: {
+			// 禁用浏览器端的 polyfill
+			"fs": false,
+			"path": false,
+			"crypto": false,
+			"stream": false,
+			"util": false,
+			"url": false,
+			"querystring": false,
+			"http": false,
+			"https": false,
+			"os": false,
+			"net": false,
+			"tls": false,
+			"zlib": false,
+			"events": false,
+			"buffer": false
+		}
+	},
 	module: {
 		rules: [
 			{
@@ -29,6 +78,17 @@ module.exports = {
 			banner: '#!/usr/bin/env node',
 			raw: true,
 		}),
+		// 添加环境变量定义
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production'),
+			'global': 'global',
+			'process': 'process'
+		}),
+		// 忽略一些可能导致问题的模块
+		new webpack.IgnorePlugin({
+			resourceRegExp: /^\.\/locale$/,
+			contextRegExp: /moment$/
+		})
 	],
 	optimization: {
 		minimize: true,
@@ -39,10 +99,32 @@ module.exports = {
 				terserOptions: {
 					compress: {
 						ecma: 2018,
+						// 添加更多压缩选项
+						drop_console: false, // 保留 console
+						drop_debugger: true,
+						pure_funcs: [], // 不移除任何函数调用
+						keep_fargs: true, // 保留函数参数
+						keep_fnames: true, // 保留函数名
 					},
-					// https://swc.rs/docs/configuration/minification#jscminifycompress
+					mangle: {
+						keep_fnames: true, // 保留函数名
+						keep_classnames: true // 保留类名
+					},
+					format: {
+						comments: false // 移除注释
+					}
 				},
 			}),
 		],
 	},
+	// 添加 node 配置
+	node: {
+		__dirname: false,
+		__filename: false,
+		global: false
+	},
+	// 添加性能配置
+	performance: {
+		hints: false
+	}
 };
